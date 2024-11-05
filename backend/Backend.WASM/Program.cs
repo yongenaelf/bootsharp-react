45c62cb23e60776fe8c6bff4e715d4345149ace2
yongen.loong <yongen.loong@aelf.io>
2024-11-06 07:59:29 +0800

    init

---
+++ backend/Backend.WASM/Program.cs
@@ -0,0 +1,22 @@
+using Bootsharp;
+using Bootsharp.Inject;
+using Microsoft.Extensions.DependencyInjection;
+
+// Application entry point for browser-wasm build target.
+// Notice, how neither domain, nor other C# backend assemblies
+// are coupled with the JavaScript interop specifics
+// and can be shared with other build targets (console, MAUI, etc).
+
+// Generate C# -> JavaScript interop handlers for specified contracts.
+[assembly: JSExport(typeof(Backend.IComputer))]
+// Generate JavaScript -> C# interop handlers for specified contracts.
+[assembly: JSImport(typeof(Backend.Prime.IPrimeUI))]
+// Group all generated JavaScript APIs under "Computer" namespace.
+[assembly: JSPreferences(Space = [".+", "Computer"])]
+
+// Perform dependency injection.
+new ServiceCollection()
+    .AddSingleton<Backend.IComputer, Backend.Prime.Prime>() // use prime computer
+    .AddBootsharp() // inject generated interop handlers
+    .BuildServiceProvider()
+    .RunBootsharp(); // initialize interop services
